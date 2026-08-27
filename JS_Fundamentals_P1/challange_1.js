0fd3c93ce9a279e99e32fb820c790a24c458be6f
saadprof <saadprof101@gmail.com>
2026-08-27 12:42:29 +0600

    Challange #3: Tip calculator coded.

--- JS_Fundamentals_P1/challange_1.js
+++ JS_Fundamentals_P1/challange_1.js
@@ -47,10 +47,35 @@ const calcKoalaAverage = (88 + 91 + 110) / 3;
 // const calcKoalaAverage = (109 + 95 + 106) / 3;
 
 console.log(calcDolphinAverage, calcKoalaAverage);
-if(calcDolphinAverage > 100 && calcDolphinAverage > calcKoalaAverage){
-    console.log(`Yey! Dolphine scored ${calcDolphinAverage}. Wins over Koalas.`);
-} else if(calcKoalaAverage > 100 && calcKoalaAverage > calcDolphinAverage){
+if (calcDolphinAverage > 100 && calcDolphinAverage > calcKoalaAverage) {
+    console.log(
+        `Yey! Dolphine scored ${calcDolphinAverage}. Wins over Koalas.`,
+    );
+} else if (calcKoalaAverage > 100 && calcKoalaAverage > calcDolphinAverage) {
     console.log(`Koala scored ${calcKoalaAverage} and they won.`);
-} else{
-    console.log(`Both team scored below the requirement point 100. Doesn't qualify for the competition.`);
+} else {
+    console.log(
+        `Both team scored below the requirement point 100. Doesn't qualify for the competition.`,
+    );
+}
+
+console.log(
+    "------------------- Coding Challange #3: Let's create a tip calculator ---------------",
+);
+// Lets make this project with plain if/else and also with ternary operator.
+const billValue = 275;
+// const billValue = 40;
+// const billValue = 430;
+if (billValue < 50) {
+    console.log(`Your bill is only ${billValue}. No need to tip.`);
+} else if (billValue >= 50 && billValue <= 300) {
+    // 20% tip
+    console.log(
+        `Your bill is ${billValue}. You have to pay ${billValue * 0.2} in tip and your total is ${billValue + billValue * 0.2}`,
+    );
+} else {
+    // 15% tip
+    console.log(
+        `Your bill is ${billValue}. You have to pay ${billValue * 0.15} in tip and your total is ${billValue + billValue * 0.15}`,
+    );
 }
