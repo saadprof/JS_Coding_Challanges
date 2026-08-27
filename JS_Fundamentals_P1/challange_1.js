2df71657e39433f7d7b506ef799e377cea2d4c5e
saadprof <saadprof101@gmail.com>
2026-08-27 12:17:02 +0600

    The chllange #1, BMI calculator is Extended with if condition statments.

--- JS_Fundamentals_P1/challange_1.js
+++ JS_Fundamentals_P1/challange_1.js
@@ -1,4 +1,6 @@
-console.log("-------------- Coding Challange #1: BMI Calculator -----------------");
+console.log(
+    "-------------- Coding Challange #1: BMI Calculator -----------------",
+);
 // const markWeight = 78;
 // const markHeight = 1.69;
 
@@ -18,3 +20,17 @@ console.log("John BMI is: ", johnBMI);
 
 const markHigherBMI = markBMI > johnBMI;
 console.log("Does mark has higher BMI than john?", markHigherBMI);
+
+console.log(
+    "-------------- Coding Challange #2: BMI Calculator (extend) -----------------",
+);
+// This is the extended version of the challange #1, so the data will be borrowed from #1
+if (markBMI > johnBMI) {
+    console.log(
+        `Marks BMI is ${markBMI}, which is higher than Johns BMI ${johnBMI}`,
+    );
+} else {
+    console.log(
+        `Johns BMI is ${johnBMI}, absolutely higher than Marks BMI ${markBMI}`,
+    );
+}
