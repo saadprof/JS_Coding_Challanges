020515a2255e99786ac0bb656798db32c06336f0
saadprof <saadprof101@gmail.com>
2026-09-04 16:46:09 +0600

    Random number generator added.

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -4,6 +4,10 @@ const checkBtn = document.querySelector(".check");
 const score = Number(document.querySelector(".score").textContent);
 const highScore = Number(document.querySelector(".highscore").textContent);
 
+// Random number generator
+let randomNum = (Math.trunc(Math.random() * 20) + 1);
+console.log(randomNum);
+
 checkBtn.addEventListener("click", function () {
     const inputGuess = document.querySelector(".guess").value;
 });
