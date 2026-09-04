97edf94df2c5c0b5fce6ad8b6d076898d2d8afde
saadprof <saadprof101@gmail.com>
2026-09-04 21:19:57 +0600

    simple game condition is added to check if input guess is equal, higher or lower to the secret number.

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -4,24 +4,35 @@ const checkBtn = document.querySelector(".check");
 let score = Number(document.querySelector(".score").textContent);
 let highScore = Number(document.querySelector(".highscore").textContent);
 let displayMessage = document.querySelector(".message");
+let number = document.querySelector(".number");
 
-// Random number generator
-let randomNum = Math.trunc(Math.random() * 20) + 1;
+// Random secret number generator
+let secretNum = Math.trunc(Math.random() * 20) + 1;
+number.textContent = secretNum;
 // console.log(randomNum);
 
 // score = 1;
 // console.log(score);
 
 checkBtn.addEventListener("click", function () {
-    const inputGuess = document.querySelector(".guess").value;
+    const inputGuess = Number(document.querySelector(".guess").value);
+    console.log(typeof inputGuess);
 
     if (score < 1) {
         displayMessage.textContent = "You've lost the game";
         return;
     }
 
-    if(!inputGuess){
-        displayMessage.textContent = "No number! Please input a number"
+    if (!inputGuess) {
+        displayMessage.textContent = "No number! Please input a number";
         return;
-    } 
+    }
+
+    if (inputGuess === secretNum) {
+        displayMessage.textContent = "Won";
+    } else if (inputGuess > secretNum) {
+        displayMessage.textContent = "Number is lower than this";
+    } else if (inputGuess < secretNum) {
+        displayMessage.textContent = "Number is higher than this";
+    }
 });
