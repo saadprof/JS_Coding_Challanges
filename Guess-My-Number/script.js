be3b36c430ead12c041c8dc0fe748d3ff4dc4038
saadprof <saadprof101@gmail.com>
2026-09-04 16:41:28 +0600

    all the essential elements selected from DOM. also a event listener on checkBtn added.

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -1,1 +1,9 @@
-'use strict';
+"use strict";
+const againBtn = document.querySelector(".again");
+const checkBtn = document.querySelector(".check");
+const score = Number(document.querySelector(".score").textContent);
+const highScore = Number(document.querySelector(".highscore").textContent);
+
+checkBtn.addEventListener("click", function () {
+    const inputGuess = document.querySelector(".guess").value;
+});
